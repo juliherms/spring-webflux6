5f37a7093ac40dd4f89abd21e61b10d5e3374766
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2023-09-27 21:17:40 -0300

    first commit

---
+++ src/main/java/com/github/juliherms/webflux/stcoktrading/model/Stock.java
@@ -0,0 +1,15 @@
+package com.github.juliherms.webflux.stcoktrading.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Stock {
+
+    private String name;
+}
---
+++ src/test/java/com/github/juliherms/webflux/stcoktrading/StcoktradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.juliherms.webflux.stcoktrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StcoktradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
